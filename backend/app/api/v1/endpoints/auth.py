6e863b28446f0afd6cd0caa4e0a7323b929fbeb1
rvijayashreeramesh <r.vijayashreeramesh@gmail.com>
2026-09-24 10:28:12 +0530

    debugging the logs to fix a issue

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -36,13 +36,15 @@ async def register(user_in: UserCreate) -> Any:
 
 @router.post("/login", response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> Any:
+
+    print("login details :", form_data.username, form_data.password)
     user_doc = await db_instance.db.users.find_one({"email": form_data.username})
+    print("user doc :", user_doc)
     if not user_doc or not verify_password(form_data.password, user_doc["hashed_password"]):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect email or password"
         )
-    
     access_token = create_access_token(subject=user_doc["email"])
     return {
         "access_token": access_token,
